5307b0f19127bd9ca725212ce16bf493d348c6e5
Luis Cerutti <luissantimcerutti@hotmail.com>
2023-02-02 17:44:55 -0300

    first commit

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProgBackend.Interfaces;
+
+namespace ProgBackend.Classes
+{
+    public abstract class Pessoa : IPessoa
+    {
+        public string? nome {get; set;}
+
+        public float rendimento {get; set;}
+
+        public string? endereço {get; set;}
+
+        public abstract float CalcularImposto (float rendimento);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.Clear();
+//Console.WriteLine("Hello, World!");
+
+
+using ProgBackend.Classes;
+
+PessoaFisica novaPF = new PessoaFisica();
+
+novaPF.nome = "Cerutti";
+novaPF.cpf = "12345678945";
+
+Console.WriteLine(novaPF.nome);
+Console.WriteLine(novaPF.cpf);
+Console.WriteLine($"----------------------------------------------");
+
+Console.WriteLine($"Nome: {novaPF.nome}  Cpf:  {novaPF.cpf} ");
+Console.WriteLine($"----------------------------------------------");
+
+
+Console.WriteLine("Nome: " +novaPF.nome+  " Cpf: "  +novaPF.cpf+"");
+
